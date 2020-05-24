5cbf6f75f493753f94dc410ed3ebe653b2dce7ff
arkumari2000 <arkumari2000@gmail.com>
2020-05-25 00:25:09 +0530

    cleaning of code and connection of some pages + Home page added

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User,Doctor,Receptionist,Patient,Appointment
+from django.utils.translation import gettext, gettext_lazy as _
+
+@admin.register(User)
+class CustomAdmin(UserAdmin):
+	fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (_('Custom Fields'), {'fields': ('user_type',)}),
+    )
+
 # Register your models here.
-admin.site.register(User)
 admin.site.register(Doctor)
 admin.site.register(Receptionist)
 admin.site.register(Patient)
--- app/urls.py
+++ app/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-	path('',views.index,name='index'),
-	path('register', views.register , name='register'),
-	path('test',views.test,name="test"),
-	path('login', LoginView.as_view(template_name='login.html'), name='login'),
-	path('logout',views.user_logout,name='logout'),
-	path('about',views.about,name='about'),
-	path('contact',views.contact,name='contact'),
-	path('appointments',views.appointments,name='appointments'),
-	path('prescription',views.prescription,name='prescription'),
-	]
+    path('', views.index, name='index'),
+    path('register', views.register, name='register'),
+    path('test', views.test, name="test"),
+    path('login', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout', views.user_logout, name='logout'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('appointments', views.appointments, name='appointments'),
+    path('prescription', views.prescription, name='prescription'),
+    path('hrdashboard', views.hrdashboard, name='hrdashboard'),
+    path('recdashboard', views.recdashboard, name='recdashboard'),
+    path('account', views.account, name='account'),
+]
