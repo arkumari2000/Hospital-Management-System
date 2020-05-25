a082cdaa53bf15720c7941797e13bc1ac2ca4987
arkumari2000 <arkumari2000@gmail.com>
2020-05-25 12:49:29 +0530

    changes in db, logo added

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# Selection
-Hospital Management System
+# HOSPITAL MANAGEMENT SYSTEM
+
+Following features are available:
+a)Doctor Database and its login( doctor’s interface)
+b)Patient Database and its login(Patient’s interface)
+c)Prescription Management
+d)Appointment Management
+e)Human Resource Database and its login(HR interface)
+g)Hospital Accounting
+h)Financial Reporting
+i)Invoicing
+k)Patient Medical History
+l)Patient Medical Files Archive
+m)Prescription
+n)Patient Payment History
+0)Receptionist Database and its login(Receptionist interface)
+
+Downloading Procedure:
+A zip file is attached, which you have extract in you computer.
+
+How to Start this project in your localhost:
+
+1. open terminal(if using mac or linux)\command prompt in windows
+2. go in the directory where you have saved this file.
+3. enter in the file by command cd Selection
+4. enter: python manage.py runserver
+   a localhost will open and now you can access and see the functionalities.
+
+Existing Id's to login and check the interface for different usertype:
+
+1. patient- id: arkumari2000
+   password: Samidha123
+2. doctor- id: doctor
+   password:Samidha123
+3. hr- id: hr
+   password:Samidha123
+   4.receptionsit- id: receptionist
+   password:Samidha123
+   Or you can register as a new patient or doctor via registration system.
+   You cannot register as hr and receptionsit.
--- app/views.py
+++ app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect, HttpResponse
-from .forms import UserRegisterForm, PatientForm, DoctorForm, UpdateForm, ReceptionistForm, PrescriptionForm,UserUpdationForm,UpdateDoctorForm,AppointmentForm
+from .forms import UserRegisterForm, PatientForm, DoctorForm, UpdateForm, ReceptionistForm, PrescriptionForm, UserUpdationForm, UpdateDoctorForm, AppointmentForm
 from django.views.generic import View
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, logout, login
 from django.http import Http404
-from .models import User,Doctor,Receptionist,Patient,Appointment,Invoice,Prescription
+from .models import User, Doctor, Receptionist, Patient, Appointment, Invoice, Prescription
 
 # Create your views here.
 
@@ -18,17 +18,18 @@ def index(request):
 def test(request):
     return render(request, 'index.html')
 
+
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
 
         if form.is_valid():
             username = form.cleaned_data.get('username')
-            user=form.save()
-            user_type=form.cleaned_data.get('user_type')
-            if user_type==2:
+            user = form.save()
+            user_type = form.cleaned_data.get('user_type')
+            if user_type == 2:
                 Patient.objects.create(person=user)
-            if user_type==1:
+            if user_type == 1:
                 Doctot.objects.create(person=user)
             messages.success(request, f'Account created for {username}!')
             return redirect('index')
@@ -52,28 +53,28 @@ def contact(request):
 
 
 def hrdashboard(request):
-    doctors=Doctor.objects.all()
-    active=Doctor.objects.filter(status=1).count()
-    patients=Patient.objects.all().count()
-    context={
-    'doctors':doctors,
-    'active':active,
-    'patients':patients,
+    doctors = Doctor.objects.all()
+    active = Doctor.objects.filter(status=1).count()
+    patients = Patient.objects.all().count()
+    context = {
+        'doctors': doctors,
+        'active': active,
+        'patients': patients,
     }
-    return render(request, 'HRdashboard.html',context)
+    return render(request, 'HRdashboard.html', context)
 
 
 def recdashboard(request):
-    appointments=Appointment.objects.all()
-    patients=Patient.objects.all()
-    approved=Appointment.objects.filter(status='AP').count()
-    context={
-    'appointments':appointments,
-    'patients':patients,
-    'approved':approved,
+    appointments = Appointment.objects.all()
+    patients = Patient.objects.all()
+    approved = Appointment.objects.filter(status='AP').count()
+    context = {
+        'appointments': appointments,
+        'patients': patients,
+        'approved': approved,
 
     }
-    return render(request, 'Rdashboard.html',context)
+    return render(request, 'Rdashboard.html', context)
 
 
 def account(request):
@@ -82,139 +83,147 @@ def account(request):
 
 @login_required(login_url='login')
 def appointments(request):
-    appointments=Appointment.objects.all()
-    context={
-    'appointments':appointments,
+    appointments = Appointment.objects.all()
+    context = {
+        'appointments': appointments,
     }
-    if request.user.user_type==1:
-        return render(request, 'appointment.html',context)
-    elif request.user.user_type==2:
-        return render(request, 'appointment.html',context)
+    if request.user.user_type == 1:
+        return render(request, 'appointment.html', context)
+    elif request.user.user_type == 2:
+        return render(request, 'appointment.html', context)
     else:
         return HttpResponse(f'user_type is {request.user.user_type} {type(request.user.user_type)}')
 
 
 @login_required(login_url='login')
 def prescription(request):
-    if request.user.user_type==1:
-        prescriptions=request.user.doctor.prescription_set.all()
-        return render(request, 'prescription.html',{'prescriptions':prescriptions,})
-    elif request.user.user_type==2:
-        prescriptions=request.user.patient.prescription_set.all()
-        return render(request, 'prescription.html',{'prescriptions':prescriptions,})
+    if request.user.user_type == 1:
+        prescriptions = request.user.doctor.prescription_set.all()
+        return render(request, 'prescription.html', {'prescriptions': prescriptions, })
+    elif request.user.user_type == 2:
+        prescriptions = request.user.patient.prescription_set.all()
+        return render(request, 'prescription.html', {'prescriptions': prescriptions, })
     else:
-        return HttpResponse('user_type is ',user_type)
+        return HttpResponse('user_type is ', user_type)
+
 
 def invoice(request):
-    invoices=request.user.patient.invoice_set.all()
+    invoices = request.user.patient.invoice_set.all()
     if not invoices:
-        invoices=("-","-","-","-")
-    return render(request, 'invoice.html',{'invoices':invoices,})
+        invoices = ("-", "-", "-", "-")
+    return render(request, 'invoice.html', {'invoices': invoices, })
 # def create_prescription(request):
-def profile(request,id=None):
-    if request.user.user_type==1:
-        doctor=request.user.doctor
-        u_form=UserUpdationForm(instance=request.user)
-        p_form=DoctorForm(instance=doctor)
-        if request.method=='POST':
-            u_form=UserUpdationForm(request.POST)
-            p_form=PatientForm(request.POST)
+
+
+def profile(request, id=None):
+    if request.user.user_type == 1:
+        doctor = request.user.doctor
+        u_form = UserUpdationForm(instance=request.user)
+        p_form = DoctorForm(instance=doctor)
+        if request.method == 'POST':
+            u_form = UserUpdationForm(request.POST)
+            p_form = PatientForm(request.POST)
             if u_form.is_valid():
-                u_form=UserUpdationForm(request.POST,instance=request.user)
-                p_form=DoctorForm(request.POST,instance=doctor)
+                u_form = UserUpdationForm(request.POST, instance=request.user)
+                p_form = DoctorForm(request.POST, instance=doctor)
                 u_form.save()
                 p_form.save()
                 return redirect('profile')
-        return render(request,'profile.html',{'u_form':u_form,'p_form':p_form})
-    elif request.user.user_type==2:
-        patient=request.user.patient
-        u_form=UserUpdationForm(instance=request.user)
-        p_form=PatientForm(instance=patient)
-        if request.method=='POST':
-            u_form=UserUpdationForm(request.POST)
-            p_form=PatientForm(request.POST)
+        return render(request, 'profile.html', {'u_form': u_form, 'p_form': p_form})
+    elif request.user.user_type == 2:
+        patient = request.user.patient
+        u_form = UserUpdationForm(instance=request.user)
+        p_form = PatientForm(instance=patient)
+        if request.method == 'POST':
+            u_form = UserUpdationForm(request.POST)
+            p_form = PatientForm(request.POST)
             if u_form.is_valid():
-                u_form=UserUpdationForm(request.POST,instance=request.user)
-                p_form=PatientForm(request.POST,instance=patient)
+                u_form = UserUpdationForm(request.POST, instance=request.user)
+                p_form = PatientForm(request.POST, instance=patient)
                 u_form.save()
                 p_form.save()
                 return redirect('profile')
-        return render(request,'profile.html',{'u_form':u_form,'p_form':p_form})
-    elif request.user.user_type==3:
-        doctor=Doctor.objects.filter(id=id).first()
-        user=doctor.person
-        u_form=UserUpdationForm(instance=doctor.person)
-        p_form=UpdateDoctorForm(instance=doctor)
-        if request.method=='POST':
-            u_form=UserUpdationForm(request.POST)
-            p_form=PatientForm(request.POST)
+        return render(request, 'profile.html', {'u_form': u_form, 'p_form': p_form})
+    elif request.user.user_type == 3:
+        doctor = Doctor.objects.filter(id=id).first()
+        user = doctor.person
+        u_form = UserUpdationForm(instance=doctor.person)
+        p_form = UpdateDoctorForm(instance=doctor)
+        if request.method == 'POST':
+            u_form = UserUpdationForm(request.POST)
+            p_form = PatientForm(request.POST)
             if u_form.is_valid():
-                u_form=UserUpdationForm(request.POST,instance=doctor.person)
-                p_form=DoctorForm(request.POST,instance=doctor)
+                u_form = UserUpdationForm(request.POST, instance=doctor.person)
+                p_form = DoctorForm(request.POST, instance=doctor)
                 u_form.save()
                 p_form.save()
                 return redirect('hrdashboard')
-        return render(request,'profile.html',{'u_form':u_form,'p_form':p_form})
-    elif request.user.user_type==4:
-        patient=Patient.objects.filter(id=id).first()
-        u_form=UserUpdationForm(instance=patient.person)
-        p_form=PatientForm(instance=patient)
-        if request.method=='POST':
-            u_form=UserUpdationForm(request.POST)
-            p_form=PatientForm(request.POST)
+        return render(request, 'profile.html', {'u_form': u_form, 'p_form': p_form})
+    elif request.user.user_type == 4:
+        patient = Patient.objects.filter(id=id).first()
+        u_form = UserUpdationForm(instance=patient.person)
+        p_form = PatientForm(instance=patient)
+        if request.method == 'POST':
+            u_form = UserUpdationForm(request.POST)
+            p_form = PatientForm(request.POST)
             if u_form.is_valid():
-                u_form=UserUpdationForm(request.POST,instance=patient.person)
-                p_form=PatientForm(request.POST,instance=patient)
+                u_form = UserUpdationForm(
+                    request.POST, instance=patient.person)
+                p_form = PatientForm(request.POST, instance=patient)
                 u_form.save()
                 p_form.save()
                 return redirect('recdashboard')
-        return render(request,'profile.html',{'u_form':u_form,'p_form':p_form})
+        return render(request, 'profile.html', {'u_form': u_form, 'p_form': p_form})
 
-def delete(request,id):
-    if request.user.user_type==3:
-        doctor=Doctor.objects.filter(id=id).first()
+
+def delete(request, id):
+    if request.user.user_type == 3:
+        doctor = Doctor.objects.filter(id=id).first()
         doctor.person.delete()
         return redirect('hrdashboard')
-    if request.user.user_type==4:
-        doctor=Patient.objects.filter(id=id).first()
+    if request.user.user_type == 4:
+        doctor = Patient.objects.filter(id=id).first()
         doctor.person.delete()
         return redirect('recdashboard')
 
+
 def create_prescription(request):
-    if request.user.user_type==1:
-        form=PrescriptionForm()
-        if request.method=='POST':
-            form=PrescriptionForm(request.POST)
-            prescription=form.save(commit=False)
-            prescription.doctor=request.user.doctor
+    if request.user.user_type == 1:
+        form = PrescriptionForm()
+        if request.method == 'POST':
+            form = PrescriptionForm(request.POST)
+            prescription = form.save(commit=False)
+            prescription.doctor = request.user.doctor
             prescription.save()
             return redirect('prescription')
-        return render(request,'presform.html',{'form':form,})
+        return render(request, 'presform.html', {'form': form, })
+
 
 def create_appointment(request):
-    if request.user.user_type==4:
-        form=AppointmentForm()
-        if request.method=='POST':
-            form=AppointmentForm(request.POST)
+    if request.user.user_type == 4:
+        form = AppointmentForm()
+        if request.method == 'POST':
+            form = AppointmentForm(request.POST)
             if form.is_valid():
                 form.save()
             return redirect('recdashboard')
-        return render(request,'appointment_form.html',{'form':form,})
+        return render(request, 'appointment_form.html', {'form': form, })
     else:
         raise Http404
 
+
 def create_patient(request):
-    if request.user.user_type==4:
-        u_form=UserUpdationForm()
-        p_form=PatientForm()
-        if request.method=='POST':
-            u_form=UserUpdationForm(request.POST)
-            p_form=PatientForm(request.POST)
+    if request.user.user_type == 4:
+        u_form = UserUpdationForm()
+        p_form = PatientForm()
+        if request.method == 'POST':
+            u_form = UserUpdationForm(request.POST)
+            p_form = PatientForm(request.POST)
             if u_form.is_valid():
-                user=u_form.save(commit=False)
-                instance=p_form.save(commit=False)
-                instance.user=user
+                user = u_form.save(commit=False)
+                instance = p_form.save(commit=False)
+                instance.user = user
                 user.save()
                 instance.save()
                 return redirect('recdashboard')
-        return render(request,'profile.html',{'u_form':u_form,'p_form':p_form})
+        return render(request, 'profile.html', {'u_form': u_form, 'p_form': p_form})
