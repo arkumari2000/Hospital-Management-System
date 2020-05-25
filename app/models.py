6f04d3834dbadf8ef23144a26638284aa6c84151
arkumari2000 <arkumari2000@gmail.com>
2020-05-25 16:12:06 +0530

    mujhse nahi ho raha

--- app/models.py
+++ app/models.py
@@ -12,7 +12,22 @@ BLOOD_CHOICES = (
     (6, 'AB-'),
     (7, 'O+'),
     (8, 'O-'),
-    )
+)
+
+GENDER_CHOICES = (
+    (1, 'Female'),
+    (2, 'Male'),
+    (3, 'Others'),
+)
+
+DEPARTMENT_CHOICES = (
+    (1, 'Eye Care'),
+    (2, 'Skin Care'),
+    (3, 'Surgery'),
+    (4, 'Physical Therapy'),
+    (5, 'Dental'),
+)
+
 
 class User(AbstractUser):
     USER_TYPE_CHOICES = (
@@ -20,15 +35,19 @@ class User(AbstractUser):
         (2, 'patient'),
         (3, 'HR'),
         (4, 'Receptionist'),
-        )
-    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES,default=2)
+    )
+    user_type = models.PositiveSmallIntegerField(
+        choices=USER_TYPE_CHOICES, default=2)
+
 
 class Doctor(models.Model):
     person = models.OneToOneField(User, on_delete=models.CASCADE)
-    Department = models.CharField(max_length=100 , default=None,blank=True)
-    Address = models.CharField(max_length=100 , default=None,blank=True)
-    Phone = models.CharField(max_length=100 , default=None,blank=True)
-    gender = models.CharField(max_length=100 , default=None,blank=True)
+    Department = models.PositiveSmallIntegerField(
+        choices=DEPARTMENT_CHOICES, default=4)
+    Address = models.CharField(max_length=100, default=None, blank=True)
+    Phone = models.CharField(max_length=100, default=None, blank=True)
+    gender = models.PositiveSmallIntegerField(
+        choices=GENDER_CHOICES, default=3)
     STATUS = (
         (1, 'Active'),
         (0, 'Not_Active'),
@@ -37,31 +56,35 @@ class Doctor(models.Model):
         choices=STATUS,
         default=1
     )
-    attendance=models.IntegerField(null=True,blank=True)
-    salary=models.IntegerField(null=True,blank=True)
+    attendance = models.IntegerField(null=True, blank=True)
+    salary = models.IntegerField(null=True, blank=True)
+
     def __str__(self):
         return self.person.username
+
 
 class Patient(models.Model):
     person = models.OneToOneField(User, on_delete=models.CASCADE)
-    Address = models.CharField(max_length=100 , blank=True,null=True)
-    Phone = models.CharField(max_length=100 , blank=True,null=True)
-    gender = models.CharField(max_length=100 , blank=True,null=True)
-    age=models.IntegerField(blank=True,default=None,null=True)
-    case_paper=models.IntegerField(blank=True,null=True)
-    records=models.FileField(upload_to='records',blank=True)
-    blood_group=models.PositiveSmallIntegerField(choices=BLOOD_CHOICES,default=7)
+    Address = models.CharField(max_length=100, blank=True, null=True)
+    Phone = models.CharField(max_length=100, blank=True, null=True)
+    gender = models.CharField(max_length=100, blank=True, null=True)
+    age = models.IntegerField(blank=True, default=None, null=True)
+    case_paper = models.IntegerField(blank=True, null=True)
+    records = models.FileField(upload_to='records', blank=True)
+    blood_group = models.PositiveSmallIntegerField(
+        choices=BLOOD_CHOICES, default=7)
 
     def __str__(self):
         return self.person.username
 
+
 class Appointment(models.Model):
-    patient = models.ForeignKey(Patient, null=True,on_delete=models.CASCADE)
-    doctor = models.ForeignKey(Doctor, default=None,on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, null=True, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, default=None, on_delete=models.CASCADE)
     date = models.DateField(("Date"), default=datetime.date.today)
     time = models.TimeField(auto_now_add=True)
-    Pending= 'PD'
-    Approved= 'AP'
+    Pending = 'PD'
+    Approved = 'AP'
     STATUS = (
         (Pending, 'Pending'),
         (Approved, 'Approved'),
@@ -78,21 +101,23 @@ class Appointment(models.Model):
     def get_absolute_url(self):
         return reverse('index')
 
+
 class Prescription(models.Model):
-    patient = models.ForeignKey(Patient, null=True,on_delete=models.CASCADE)
-    doctor = models.ForeignKey(Doctor, default=None,on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, null=True, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, default=None, on_delete=models.CASCADE)
     date = models.DateField(("Date"), default=datetime.date.today)
     Symptoms = models.CharField(max_length=100)
     Description = models.TextField()
 
 # class Invoice
 
+
 class Invoice(models.Model):
-    patient = models.ForeignKey(Patient, null=True,on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, null=True, on_delete=models.CASCADE)
     date = models.DateField(("Date"), default=datetime.date.today)
-    remain=models.IntegerField()
-    paid=models.IntegerField()
-    link=models.FileField(upload_to='invoices',blank=True)
+    remain = models.IntegerField()
+    paid = models.IntegerField()
+    link = models.FileField(upload_to='invoices', blank=True)
 
     def __str__(self):
         return str(self.patient)
